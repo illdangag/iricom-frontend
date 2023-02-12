23ddee6be5dc27d303c1565454576de04f840e50
illdangag <illdangag@gmail.com>
2023-02-12 18:10:40 +0900

    init recoil

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,31 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import { Button, ButtonGroup } from '@chakra-ui/react'
+import { useRecoilValue, useSetRecoilState, } from 'recoil';
+import testCountAtom, { increaseTestCount, } from '../recoil/testCount';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-    <ButtonGroup variant='outline' spacing='6'>
-      <Button colorScheme='blue'>Save</Button>
-      <Button>Cancel</Button>
-    </ButtonGroup>
-  </Layout>
-)
+const IndexPage = () => {
+  const testCount = useRecoilValue(testCountAtom);
+  const setIncreaseTestCount = useSetRecoilState(increaseTestCount);
+
+  const onClickIncreaseButton = () => {
+    setIncreaseTestCount(100);
+  };
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">About</Link>
+      </p>
+      <p>
+        {testCount?.count}
+      </p>
+      <ButtonGroup variant='outline' spacing='6'>
+        <Button colorScheme='blue' onClick={onClickIncreaseButton}>increase</Button>
+      </ButtonGroup>
+    </Layout>
+  )
+}
 
 export default IndexPage
